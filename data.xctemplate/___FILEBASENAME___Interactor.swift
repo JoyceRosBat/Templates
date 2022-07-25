4a9ba55c1279541100db4fe8dc274546afb3364b
Joyce Rosario Batista <joyce.rosbat@gmail.com>
2022-07-25 08:50:08 +0100

    Add Data template

---
+++ data.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,30 @@
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//  ___COPYRIGHT___
+//
+//  Template by Joyce Rosario Batista
+//
+
+import Foundation
+
+protocol ___VARIABLE_dataModuleName___InteractorProtocol {
+     
+}
+
+class ___VARIABLE_dataModuleName___Interactor: ___VARIABLE_dataModuleName___InteractorProtocol {
+    
+    // MARK: - Properties
+    
+    var repository: ___VARIABLE_dataModuleName___RepositoryProtocol
+    
+    // MARK: - Init
+    
+    init(repository: ___VARIABLE_dataModuleName___RepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    // MARK: - ___VARIABLE_dataModuleName___Interactor functions
+    
+}
---
+++ data.xctemplate/___FILEBASENAME___Repository.swift
@@ -0,0 +1,28 @@
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//  ___COPYRIGHT___
+//
+//  Template by Joyce Rosario Batista
+//
+
+import Foundation
+
+protocol ___VARIABLE_dataModuleName___RepositoryProtocol {
+     
+}
+
+class ___VARIABLE_dataModuleName___Repository: ___VARIABLE_dataModuleName___RepositoryProtocol {
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    init() {
+        
+    }
+    
+    // MARK: - ___VARIABLE_dataModuleName___RepositoryProtocol Functions
+    
+}
